bb3a68b1bee8de526aef037bba8213fc7fac2942
pabu <house1583@naver.com>
2026-08-04 13:04:22 +0900

    member end

--- src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
@@ -1,10 +1,13 @@
 package jpabook.jpashop.domain.service;
 
+import jakarta.persistence.EntityManager;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +18,7 @@ class MemberServiceTest {
 
     @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
 
     @Test
     public void 회원가입() throws Exception {
@@ -32,9 +36,16 @@ class MemberServiceTest {
     @Test
     public void 중복_회원_예외() throws Exception {
         //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
 
         //when
+        memberService.join(member1);
 
         //then
+        Assertions.assertThrows(IllegalStateException.class, () -> memberService.join(member2));
     }
 }
